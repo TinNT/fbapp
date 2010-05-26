8755e533c188b7b505731210afc6d81f8542ba17
mic <mic@Tokyo.(none)>
2010-05-26 15:12:58 +0200

    invitations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  # filter_parameter_logging :passwordhelper :all # include all helpers, all the time
+
+  #ensure_authenticated_to_facebook
+
 end
