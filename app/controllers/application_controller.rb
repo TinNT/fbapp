433be081314d539afaa4a84454929da9848ba1c9
mic <mic@Tokyo.(none)>
2010-05-27 17:27:50 +0200

    session test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,10 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :passwordhelper :all # include all helpers, all the time
-
+  before_filter :setup
   #ensure_authenticated_to_facebook
-
+  
+  def setup
+    @session = Facebooker::Session.create('d20d83a6da0ee2791615a33589394829','36a703ad556f1ddf696181015ed4bb34')
+  end
 end
